744bf202717750b17f154eed2bbbc582c29cf91a
bingeing.yang <bing2840@@@>
2023-11-29 23:07:29 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,51 @@
 <script setup lang="ts">
+import { ref, watchEffect } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { decompressFromEncodedURIComponent, compressToEncodedURIComponent } from 'lz-string';
+const str = ref('');
+const jsonObj = ref('');
+watchEffect(() => {
+  jsonObj.value = '';
+  if (str.value) {
+    let jsonData = decompressFromEncodedURIComponent(str.value);
+    jsonObj.value = JSON.stringify(JSON.parse(jsonData), null, 4);
+  }
+});
+const jsonStr = ref('');
+const lzStr = ref('');
+watchEffect(() => {
+  lzStr.value = '';
+  if (jsonStr.value) {
+    let jsonData = compressToEncodedURIComponent(JSON.stringify(JSON.parse(jsonStr.value)));
+    lzStr.value = jsonData;
+  }
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="box">
+  <div class="">str -> json</div>
+  <div style="width: 90%;">
+    <n-input v-model:value="str" style="width: 100%;" rows="10" type="textarea" placeholder="编码字符串" />
+  </div>
+  <div style="height: 520px;width: 90%;">
+    <d-code-editor v-model="jsonObj" :options="{ language: 'json' }" style="height: 520px;width: 100%;"></d-code-editor>
+  </div>
+  <div class="">str -> json</div>
+  <div style="width: 90%;">
+    <n-input v-model:value="jsonStr" style="width: 100%;" rows="10" type="textarea" placeholder="json string" />
+  </div>
+  <div style="width: 90%;word-break: break-all;">
+    {{ lzStr }}
+  </div>
+</div>
+  <!-- <RouterView /> -->
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+<style scoped lang="less">
+.box{
+  padding: 30px;
+  div {
+    border: 1px solid green;
   }
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import { DevUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
+        }
+      ]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver(), DevUiResolver()]
+    })
   ],
   resolve: {
     alias: {
